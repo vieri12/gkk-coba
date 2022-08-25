a095c56e56f974f2e6d75faa657f093d98f25b1a
vieri12 <vieribachtiar15@gmail.com>
2022-08-25 22:19:23 +0700

    smua

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        title: "#2A3890",
+        dark_text: "#111827",
+        quots: "#435DB8",
         primary: '#2A3890',
         dark: '#DEDEDE',
-      }
+      },
+      letterSpacing: {
+        wide: ".075em",
+      },
     },
   },
   plugins: [],
